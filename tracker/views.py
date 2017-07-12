785995d7de4de27b8ddc192aa9fc5a6f9f6b4043
thagler1 <tdhagler1@gmail.com>
2017-07-12 04:57:35 -0500

    added bootstrap

--- tracker/views.py
+++ tracker/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .utils.ftptest import update_data
+from .utils.ftpscrape import update_data
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
 # Create your views here.
 def update(request):
-    update_data()
+    #update_data()
     template = loader.get_template('index.html')
     context ={}
     return HttpResponse(template.render(context, request))
