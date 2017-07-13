6a649a109d25587aaa4fdfa94defd00cb9b271f8
thagler1 <tdhagler1@gmail.com>
2017-07-13 15:08:50 -0500

    added basin stats and verbose region name

--- hurricane_tracker/settings_secret.py
+++
@@ -1,7 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm5qa!!rx7pvmt6eby=h$o$t)v@1xmbftjg16r4=mz9e+u36uxz'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
