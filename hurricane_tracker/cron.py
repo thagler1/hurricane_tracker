192cb024224e7588a7a72d4f3ef05e9a696d3a5e
thagler1 <tdhagler1@gmail.com>
2017-07-17 19:20:03 -0500

    testfile

--- hurricane_tracker/cron.py
+++ hurricane_tracker/cron.py
@@ -5,9 +5,8 @@ import os
 def noaa_ftp():
     update_data()
     return True
+
+
 def test():
-    x = Storm.objects.all().count()
-    file = open(r'testfile.txt', 'w')
-    file.write('Hello World')
-    file.write("there are %s storms"%(x))
-    file.close()
+    print("Hello")
+    return 0
