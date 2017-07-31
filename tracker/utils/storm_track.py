84d28ab56aeb5f2517b09add8a04a58312fe8c8b
Todd Hagler <tdhagler1@gmail.com>
2017-07-31 04:26:59 -0500

    storm path functions

--- tracker/utils/storm_track.py
+++ tracker/utils/storm_track.py
@@ -9,7 +9,7 @@ def add_track_init():
             if storm.path:
                 storm.path.append((a.lat,a.long))
             else:
-                storm.path =LineString((a.lat,a.long), srid=4326)
+                storm.path =LineString([a.lat,a.long])
 
         print(storm.path)
         storm.save()
