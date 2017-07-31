dfd39bf1897c9843c2d25f71141ee24742750e7c
Todd Hagler <tdhagler1@gmail.com>
2017-07-31 04:00:40 -0500

    storm path functions

--- tracker/utils/storm_track.py
+++ tracker/utils/storm_track.py
@@ -3,7 +3,7 @@ from ..models import Storm, Advisory
 def add_track_init():
     storms = Storm.objects.filter(path=None)
     for storm in storms:
-        advs = Advisory.objects.filter(stormid=storm._id).order_by('date')
+        advs = Advisory.objects.filter(stormid=storm).order_by('date')
         path = [(a.lat, a.long) for a in advs]
         storm.path = path
         print(path)
