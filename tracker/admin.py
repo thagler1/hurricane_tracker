385b372ea5ca382be228b67ed6315988e8f5dcc1
Todd Hagler <tdhagler1@gmail.com>
2017-07-31 13:51:01 -0500

    storm path functions

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.gis import admin
 from .models import Storm, Advisory, Posts
 # Register your models here.
 
-admin.site.register(Storm)
+admin.site.register(Storm,  admin.GeoModelAdmin)
 admin.site.register(Advisory)
 admin.site.register(Posts)
